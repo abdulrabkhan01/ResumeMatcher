396d03aa054a8882673ca76a0569d1e8592de255
abdulrabkhan01 <abdul.lambent@gmail.com>
2022-06-14 19:21:12 +0530

    Resume Rating Microservice added

---
+++ microservices/ResumeRatingService/src/main/java/com/ark/ResumeScanService/ResumeScanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ark.ResumeScanService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeScanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeScanServiceApplication.class, args);
+	}
+
+}
