5acf06b7c0fff2b2abe0d4c7d648db7246c72f65
abdulrabkhan01 <abdul.lambent@gmail.com>
2022-06-14 19:24:51 +0530

    Added Resume Scan Service, this service will parse resume and stored the scanned result in database for further processing

---
+++ microservices/ResumeScanService/src/test/java/com/ark/ResumeScanService/ResumeScanServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ark.ResumeScanService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+		properties = {"spring.cloud.config.enabled=false"}
+)
+class ResumeScanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
