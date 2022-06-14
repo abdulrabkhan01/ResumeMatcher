7e2de3685659bbec56a153ca127c387fd2fd26dd
abdulrabkhan01 <abdul.lambent@gmail.com>
2022-06-14 18:47:41 +0530

    Starting new microservice based app for resume scanning and matching with the job requirements present in the JD

---
+++ microservices/ResumeMatchingService/src/test/java/com/ark/ResumeScanService/ResumeScanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ark.ResumeScanService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeScanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
